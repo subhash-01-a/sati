373d16ddbba84a030342cfa00ed806e8e7d1bb45
subhash-01-a <subhashtakshasila@gmail.com>
2025-11-24 15:31:46 +0530

    Update exam.java

--- exam.java
+++ exam.java
@@ -1,21 +1,3 @@
-//to efer current class instance  variable
-class student{
-int age;
-String name;
-
-student(String name,int age)
-{
-this.name=name;
-this.age=age;
-
-}
-
- void display(){
- System.out.println("name"+this.name+"age"+this.age);
- }
- }
- 
- }
 public class Exam {
     public static void main(String[] args) {
         System.out.println("Hello World");
